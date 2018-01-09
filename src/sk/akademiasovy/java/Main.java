17ed8ed2ee744a3819f4e975d3aa00402bfc3bec
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-01-09 12:06:31 +0100

    added generation of plate

--- src/sk/akademiasovy/java/Main.java
+++ src/sk/akademiasovy/java/Main.java
@@ -16,6 +16,22 @@ public class Main {
        car2.printInfo();
         System.out.println("Cost: KE<-->Prag:"+ car2.getPrice(1600,1.33f));
 
+        car1.increaseSpeed();
+        car1.increaseSpeed(85);
+        car2.increaseSpeed(75);
+
+        if(car2.getSpeed()>car1.getSpeed())
+            System.out.println("Skoda 120L is faster");
+        else if (car1.getSpeed()<car2.getSpeed())
+            System.out.println("Audi A8 is faster");
+            else if (car1.getSpeed()==car2.getSpeed())
+        System.out.println("Both cars have the same speed");
+
+       Car car3;
+        car3=car2;
+        car3.stop();
+
+        car3=new Car("Seat Ibiza",7.1f,"trencin");
     }
 }
 
