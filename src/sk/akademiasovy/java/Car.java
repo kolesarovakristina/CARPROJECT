917f25f2b6f8081bc00d62f930249728ccd048ef
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-01-10 08:40:49 +0100

    added generation of plate

--- src/sk/akademiasovy/java/Car.java
+++ src/sk/akademiasovy/java/Car.java
@@ -1,10 +1,14 @@
 package sk.akademiasovy.java;
 
+import java.util.Random;
+
 public class Car {
     private String brand;
     private int speed;
     private boolean engine;
     private float gasConsumption;
+    private boolean frontWiper;
+    private String plate;
     //constructor
 
     public Car(){
@@ -12,6 +16,8 @@ public class Car {
         engine=false;
         gasConsumption=7.9f;
         brand=null;
+        frontWiper=false;
+        generatePlate(null);
     }
 
     public Car(String brand,float consumption){
@@ -19,7 +25,42 @@ public class Car {
         engine=false;
         gasConsumption=consumption;
         this.brand=brand;
+        frontWiper=false;
+        generatePlate(null);
     }
+    public Car(String brand, float consumption, String city){
+        speed=0;
+        engine=false;
+        gasConsumption=consumption;
+        this.brand=brand;
+        frontWiper=false;
+        generatePlate(city);
+    }
+    private void generatePlate(String city){
+        if (city==null)
+            city="";
+        switch(city.toLowerCase().trim()){
+            case "trencin": plate= "TN-"; break;
+            case "presov": plate= "PO-"; break;
+            case "bratislava": plate= "BA-"; break;
+            case "nitra": plate= "NR-"; break;
+            case "senec": plate= "SC-"; break;
+            default:plate= "KE-";
+        }
+        Random random=new Random();
+        int i;
+        for(i=1;i<=3;i++){
+            int randomNumber=random.nextInt(10);
+            plate=plate+randomNumber;
+        }
+        for(i=1;i<=2;i++){
+            int randomNumber= random.nextInt(26)+'A';
+            plate=plate+(char)randomNumber;
+        }
+        System.out.println("Assigned plate:"+plate);
+
+    }
+
     public void setBrand(String value){
         brand=value;
     }
@@ -27,7 +68,7 @@ public class Car {
         this.gasConsumption=gasConsumption;
     }
     public void printInfo(){
-        System.out.println("Brand:"+brand+" Concumption:"+gasConsumption+" Actual speed:"+speed+" Engine:"+engine);
+        System.out.println("Brand:"+brand+" Consumption:"+gasConsumption+" Actual speed:"+speed+" Engine:"+engine);
     }
     public void startEngine(){
         engine=true;
@@ -38,7 +79,11 @@ public class Car {
     }
     public void increaseSpeed(){
         if(engine)
-        speed+=10;
+            speed+=10;
+    }
+    public void increaseSpeed(int value){
+        if(engine && value>0)
+        speed+=value;
     }
     public void decreaseSpeed(){
         speed-=10;
@@ -52,6 +97,19 @@ public class Car {
         float price;
         price=distance*gasConsumption*costPerLiter/100;
         return price;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void turnOnFrontWiper(){
+        frontWiper=true;
+    }
+
+    public void turnOfFrontWiper(){
+        frontWiper=false;
+
     }
 
 }
